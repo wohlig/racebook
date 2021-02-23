64d602edbf0cbaa05e811dc4e41ec49875bcd4ca
Siddhesh Salunkhe <siddhesh.salunkhe@wohlig.com>
2021-02-23 12:05:38 +0530

    add global files

--- config/globals.js
+++ config/globals.js
@@ -2,3 +2,19 @@
  * Define Global Variables Here
  * global._ = require("lodash")
  */
+
+if (process.env["mongodb_url"]) {
+    console.log("INSIDE IFFFFF")
+    var mongodbTemplate = process.env["mongodb_url"]
+    var mongodbUsername = process.env["mongodb_username"]
+    var mongodbPassword = process.env["mongodb_password"]
+    console.log(mongodbTemplate)
+    global.env.MONGODB_URL = _.template(mongodbTemplate)({
+        databaseName: `${process.env["product_name"]}-racebook-${process.env["product_namespace"]}`,
+        databaseUsername: mongodbUsername,
+        databasePassword: mongodbPassword
+    })
+    console.log(global.env.MONGODB_URL)
+
+    global.env.PORT = 3000
+}
