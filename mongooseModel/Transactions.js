b1ccfbc2c0a0051ee2764f75bfbdba5955bb0e37
Siddhesh Salunkhe <siddhesh.salunkhe@wohlig.com>
2021-06-01 15:21:32 +0530

    schema changes for debit call

--- mongooseModel/Transactions.js
+++ mongooseModel/Transactions.js
@@ -1,63 +1,87 @@
-var schema = new Schema(
-    {
-        sid: String,
-        userId: {
-            type: Schema.Types.ObjectId,
-            ref: "User"
+var schema = new Schema({
+    sid: String,
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    currency: {
+        type: String
+    },
+    rate: {
+        type: Number,
+        default: 1
+    },
+    winLoseAmt: {
+        type: Number
+    },
+    game: {},
+    subGame: {
+        type: String
+    },
+    url: String,
+    transaction: {
+        id: {
+            type: String,
+            index: true
         },
-        currency: {
+        refId: {
+            type: String,
+            index: true
+        },
+        amount: {
             type: String
         },
-        rate: {
-            type: Number,
-            default: 1
-        },
-        winLoseAmt: {
+        odds: {
             type: Number
         },
-        game: {},
-        subGame: {
+        bettype: {
             type: String
         },
-        url: String,
-        transaction: {
-            id: {
-                type: String,
-                index: true
-            },
-            refId: {
-                type: String,
-                index: true
-            },
-            amount: String
+        timeofbet: {
+            type: Date
         },
-        type: String,
-        playId: String,
-        transactionSentToMainServer: {
-            type: Boolean,
-            default: false
+        RunnerName: {
+            type: String
         },
-        ValueAsNumber: {
+        MeetingName: {
+            type: String
+        },
+        MeetingID: {
+            type: String
+        },
+        EventNo: {
             type: Number
         },
-        bets: {
-            type: Array
-        },
-        result: {
-            type: Schema.Types.Mixed
-        },
-        detailInfo: {
-            type: Schema.Types.Mixed
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now
-        },
-        updatedAt: {
-            type: Date,
-            default: Date.now
+        RunnerNo: {
+            type: Number
         }
+    },
+    type: String,
+    playId: String,
+    transactionSentToMainServer: {
+        type: Boolean,
+        default: false
+    },
+    ValueAsNumber: {
+        type: Number
+    },
+    bets: {
+        type: Array
+    },
+    result: {
+        type: Schema.Types.Mixed
+    },
+    detailInfo: {
+        type: Schema.Types.Mixed
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
     }
-)
+})
 
 export default mongoose.model("Transactions", schema)
